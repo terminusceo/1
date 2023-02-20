5a0a8c72a74046a6decd09630227f3d561fa102d
terminusceo <125861517+terminusceo@users.noreply.github.com>
2023-02-20 12:28:57 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -323,14 +323,7 @@ function App() {
                 color: "var(--accent-text)",
               }}
             >
-              {truncate(
-                new Date(timeRemaining).toLocaleString("en-GB", {
-                  hour: "numeric",
-                  minute: "numeric",
-                  second: "numeric",
-                }),
-                10
-              )}
+              MINT OPEN
             </s.TextDescription>
 
 
